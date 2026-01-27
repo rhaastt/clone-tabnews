3b8f56260351a62342da904efde6ea82a94dd0ef
Luis Miguel <87282690+rhaastt@users.noreply.github.com>
2026-01-27 22:27:38 +0000

    database e env

---
+++ infra/database.js
@@ -0,0 +1,19 @@
+import { Clien, Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    database: "mydatabase",
+    password: "example",
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({ status: "ok" });
+import database from "../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result);
+  response.status(200).json({ chave: "ok" });
 }
 
 export default status;
