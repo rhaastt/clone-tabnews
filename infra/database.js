1716b76d18095f5c45234b4cbd87a43dcb369ebd
Luis Miguel <87282690+rhaastt@users.noreply.github.com>
2026-02-05 18:59:30 +0000

    database js mais robusto ao lidar com erros

--- infra/database.js
+++ infra/database.js
@@ -1,17 +1,23 @@
-import { Clien, Client } from "pg";
+import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    database: "mydatabase",
-    password: "example",
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
