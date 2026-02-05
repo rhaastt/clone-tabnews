99e7d5f8f79428ba83bbcf1317f1115719b6cf2a
Luis Miguel <87282690+rhaastt@users.noreply.github.com>
2026-02-05 18:59:49 +0000

    finaliza endpoint de status

--- tests/integration/get.test.js
+++ tests/integration/get.test.js
@@ -1,4 +1,28 @@
-test("GET to /api/v1/status should return 200", async () => {
+test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  console.log("Data de atualização: " + parsedUpdatedAt);
+
+  expect(responseBody.dependecies.database.version).toEqual("16.0");
+  console.log(
+    "Versão do banco de dados deve ser: " +
+      responseBody.dependecies.database.version,
+  );
+
+  expect(responseBody.dependecies.database.max_connections).toEqual(100);
+  console.log(
+    "O maximo de conexões deve ser: " +
+      responseBody.dependecies.database.max_connections,
+  );
+
+  expect(responseBody.dependecies.database.opened_connections).toEqual(1);
+  console.log(
+    "Conexões abertas deve ser: " +
+      responseBody.dependecies.database.opened_connections,
+  );
 });
